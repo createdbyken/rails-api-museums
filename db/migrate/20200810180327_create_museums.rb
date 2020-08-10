217999fbebf95aeafdcdfea85ebd33e4ac24fe6a
ken <harygato3@gmail.com>
2020-08-10 13:27:53 -0500

    added model for Museums and created migrations

---
+++ db/migrate/20200810180327_create_museums.rb
@@ -0,0 +1,14 @@
+class CreateMuseums < ActiveRecord::Migration[5.2]
+  def change
+    create_table :museums do |t|
+      t.string :name
+      t.string :city
+      t.string :state
+      t.string :categories
+      t.string :description
+      
+
+      t.timestamps
+    end
+  end
+end
