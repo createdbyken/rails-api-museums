7dd67d9ee5090e91a47874bfadaf724ec3ebe18e
ken <harygato3@gmail.com>
2020-08-10 17:14:31 -0500

    Extracting name, location, type and desc to the database using a hash

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -30,10 +30,31 @@ class Scraper
 
             museums_list << doc.css('.itemGroup').css('.item').css('.basic')
         end
+        create_museums(museums_list)
     end
 
-    def create_museums(museums_list) 
+    def create_museums(museums_list)
+        museums_list.each do |museum|
+            name = museum.css('.source').css('a').text
+            location = museum.css('.location').text.split(', ')
+            type = museum.css('.type').text
+            desc = museum.css('.abstract').text
+
+            museum_info = {
+                name: name,
+                city: location[0],
+                state: location[1],
+                categories: type,
+                description: desc
+            }
+
+            museums << museum_info
+        end
+
+        museums
+
     end
+    
 end
 
 scrape = Scraper.new
